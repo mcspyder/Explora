2139462ff60ed9df6bb349e57e329569b6210fc9
Nara <mills.nargiz@gmail.com>
2025-01-20 19:49:01 +0100

    Fragment sample

--- app/src/main/java/com/example/explora2025/MainActivity.kt
+++ app/src/main/java/com/example/explora2025/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.explora2025
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+
 
 class MainActivity : AppCompatActivity() {
-
+    private var isFragmentA = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    override fun onBackPressed() {
-        finishAffinity()
+
+        // Initialize with FragmentA
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, FragmentA())
+            .commit()
+
+        findViewById<MaterialButton>(R.id.switch_button).setOnClickListener {
+            val fragment: Fragment = if (isFragmentA) FragmentB() else FragmentA()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+            isFragmentA = !isFragmentA
+        }
     }
 }
